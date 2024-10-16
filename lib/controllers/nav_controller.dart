4fd7eae7a2f8586694fab630283d522333bac0ab
Zianur <mdzianurjoy@gmail.com>
2024-10-17 00:19:11 +0600

    implemented bottomappbar with getx simple state management

--- lib/controllers/nav_controller.dart
+++ lib/controllers/nav_controller.dart
@@ -2,12 +2,18 @@ import 'package:get/get.dart';
 
 class BottomNavController extends GetxController {
 
+  ///implementing with reactive state management
   // Observable variable for current tab index
-  var selectedIndex = 0.obs;
+  // var selectedIndex = 0.obs;
+
+
+  ///implementing with simple state management
+  var selectedIndex = 0;
 
   // Method to change the index
   void changeTabIndex(int index) {
-    selectedIndex.value = index;
+    selectedIndex = index;
+    update();
   }
 
 
--- lib/screens/bottom_nav_bar.dart
+++ lib/screens/bottom_nav_bar.dart
@@ -1,4 +1,5 @@
 import 'package:design_food_delivery_app/controllers/nav_controller.dart';
+import 'package:design_food_delivery_app/screens/home.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -17,18 +18,18 @@ class _BottomNavBarState extends State<BottomNavBar> {
   Widget build(BuildContext context) {
     return Scaffold(
 
-      body: Obx(() {
-        // This will reactively update the screen based on the selected index
+      body: GetBuilder(
+        builder: (BottomNavController controller){
         return IndexedStack(
-          index: bottomNavController.selectedIndex.value,
+          index: bottomNavController.selectedIndex,
           children: [
-            Center(child: Text('Home Page')),
+            Home(),
             Center(child: Text('Favorite Page')),
             Center(child: Text('Edit Note Page')),
             Center(child: Text('Menu Page')),
           ],
         );
-      }),
+      },),
 
 
       floatingActionButton: FloatingActionButton.large(
@@ -56,11 +57,13 @@ class _BottomNavBarState extends State<BottomNavBar> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 IconButton(
-                  icon: const Icon(
-                    Icons.home,
-                    color: Colors.black,
-                    size: 30,
-                  ),
+                  icon: GetBuilder(
+                      builder: (BottomNavController controller){
+                        return Icon(
+                          Icons.home,
+                          color: bottomNavController.selectedIndex == 0 ? Colors.green : Colors.black,
+                        );
+                  }),
                   onPressed: () {
                     bottomNavController.changeTabIndex(0);
                   },
@@ -69,11 +72,13 @@ class _BottomNavBarState extends State<BottomNavBar> {
                 Padding(
                   padding: const EdgeInsets.only(left: 30),
                   child: IconButton(
-                    icon: const Icon(
-                      Icons.favorite,
-                      color: Colors.black,
-                      size: 30,
-                    ),
+                    icon: GetBuilder(
+                        builder: (BottomNavController controller){
+                          return Icon(
+                            Icons.favorite,
+                            color: bottomNavController.selectedIndex == 1 ? Colors.green : Colors.black,
+                          );
+                        }),
                     onPressed: () {
                       bottomNavController.changeTabIndex(1);
                     },
@@ -90,11 +95,13 @@ class _BottomNavBarState extends State<BottomNavBar> {
                 Padding(
                   padding: const EdgeInsets.only(right: 30),
                   child: IconButton(
-                    icon: const Icon(
-                      Icons.edit_note,
-                      color: Colors.black,
-                      size: 30,
-                    ),
+                    icon: GetBuilder(
+                        builder: (BottomNavController controller){
+                          return Icon(
+                            Icons.edit_note_rounded,
+                            color: bottomNavController.selectedIndex == 2 ? Colors.green : Colors.black,
+                          );
+                        }),
                     onPressed: () {
                       bottomNavController.changeTabIndex(2);
                     },
@@ -103,11 +110,13 @@ class _BottomNavBarState extends State<BottomNavBar> {
               ],
             ),
             IconButton(
-              icon: const Icon(
-                Icons.menu,
-                color: Colors.black,
-                size: 30,
-              ),
+              icon: GetBuilder(
+                  builder: (BottomNavController controller){
+                    return Icon(
+                      Icons.menu,
+                      color: bottomNavController.selectedIndex == 3 ? Colors.green : Colors.black,
+                    );
+                  }),
               onPressed: () {
                 bottomNavController.changeTabIndex(3);
               },
@@ -116,5 +125,122 @@ class _BottomNavBarState extends State<BottomNavBar> {
         ),
       ),
     );
+
+
+
+    ///implementing with reactive state management
+    //   body: Obx(() {
+    //     // This will reactively update the screen based on the selected index
+    //     //this is used to show selected widget
+    //     //index refers to position of the widget defined in it
+    //     return IndexedStack(
+    //       index: bottomNavController.selectedIndex.value,
+    //       children: const [
+    //         Home(),
+    //         Center(child: Text('Favorite Page')),
+    //         Center(child: Text('Edit Note Page')),
+    //         Center(child: Text('Menu Page')),
+    //       ],
+    //     );
+    //   }),
+    //
+    //
+    //   floatingActionButton: FloatingActionButton.large(
+    //     backgroundColor: Colors.green,
+    //     onPressed: () {},
+    //     child: const Icon(Icons.local_grocery_store, color: Colors.white,),
+    //     shape: RoundedRectangleBorder(
+    //       borderRadius: BorderRadius.circular(100),
+    //     ),
+    //   ),
+    //   floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    //
+    //
+    //   bottomNavigationBar: BottomAppBar(
+    //     elevation: 5,
+    //     shape: const CircularNotchedRectangle(),
+    //     padding: const EdgeInsets.symmetric(horizontal: 10),
+    //     height: 60,
+    //     color: Colors.white,
+    //     notchMargin: 5,
+    //     child: Row(
+    //       mainAxisSize: MainAxisSize.max,
+    //       children: [
+    //         Row(
+    //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //           children: [
+    //             IconButton(
+    //               icon: Obx(() {
+    //                   return Icon(
+    //                     Icons.home,
+    //                     color: bottomNavController.selectedIndex.value == 0 ? Colors.green : Colors.black,
+    //                     size: 30,
+    //                   );
+    //                 }
+    //               ),
+    //               onPressed: () {
+    //                 bottomNavController.changeTabIndex(0);
+    //               },
+    //             ),
+    //
+    //             Padding(
+    //               padding: const EdgeInsets.only(left: 30),
+    //               child: IconButton(
+    //                 icon: Obx(() {
+    //                     return Icon(
+    //                       Icons.favorite,
+    //                       color: bottomNavController.selectedIndex.value == 1 ? Colors.green : Colors.black,
+    //                       size: 30,
+    //                     );
+    //                   }
+    //                 ),
+    //                 onPressed: () {
+    //                   bottomNavController.changeTabIndex(1);
+    //                 },
+    //               ),
+    //             ),
+    //           ],
+    //         ),
+    //
+    //         Expanded(child: Text('')),
+    //
+    //         Row(
+    //           mainAxisAlignment: MainAxisAlignment.spaceAround,
+    //           children: [
+    //             Padding(
+    //               padding: const EdgeInsets.only(right: 30),
+    //               child: IconButton(
+    //                 icon: Obx(() {
+    //                     return Icon(
+    //                       Icons.edit_note,
+    //                       color: bottomNavController.selectedIndex.value == 2 ? Colors.green : Colors.black,
+    //                       size: 30,
+    //                     );
+    //                   }
+    //                 ),
+    //                 onPressed: () {
+    //                   bottomNavController.changeTabIndex(2);
+    //                 },
+    //               ),
+    //             ),
+    //           ],
+    //         ),
+    //         IconButton(
+    //           icon: Obx(() {
+    //               return Icon(
+    //                 Icons.menu,
+    //                 color: bottomNavController.selectedIndex.value == 3 ? Colors.green : Colors.black,
+    //                 size: 30,
+    //               );
+    //             }
+    //           ),
+    //           onPressed: () {
+    //             bottomNavController.changeTabIndex(3);
+    //           },
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 }
