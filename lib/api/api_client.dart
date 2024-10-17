41d2def913212ba8aea5e12a8c9ba2ecc8c7d14a
zianur-6am <zianur.6amtech@gmail.com>
2024-10-17 18:35:24 +0600

    designed category and popularfood

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -1,16 +1,16 @@
-import 'package:design_food_delivery_app/utils/app_constant.dart';
-import 'package:get/get_connect/http/src/response/response.dart';
-import 'package:http/http.dart' as http;
-
-
-class ApiClient{
-  // static var client = http.Client();
-
-  // static Future<http.Response> fetchProducts() async {
-  //   http.Response response = await http.get(
-  //     Uri.parse('${AppContants.baseUrl}${AppContants.configUri}'),
-  //   );
-  //   return response;
-  // }
-
-}
+// import 'package:design_food_delivery_app/utils/app_constant.dart';
+// import 'package:get/get_connect/http/src/response/response.dart';
+// import 'package:http/http.dart' as http;
+//
+//
+// class ApiClient{
+//   // static var client = http.Client();
+//
+//   // static Future<http.Response> fetchProducts() async {
+//   //   http.Response response = await http.get(
+//   //     Uri.parse('${AppContants.baseUrl}${AppContants.configUri}'),
+//   //   );
+//   //   return response;
+//   // }
+//
+// }
